3f868420078df1721fd1380a55c9af0f6cbbc408
KevEva <148101859+KevEva@users.noreply.github.com>
2024-10-08 13:53:37 +1100

    Error fix and addition of addition json.

--- TestHandler.cs
+++ TestHandler.cs
@@ -18,42 +18,49 @@ public class TestHandler : MonoBehaviour
     //User input
     private string input;
     //Location Database
-    private LocationDataPoints _location;
+    private LocationDatas _location;
 
 
     void Start()
     {
         //Get responses from json
-        string json = File.ReadAllText(Application.dataPath + "/ResponseDataFile.json");
-        _responses = JsonUtility.FromJson<EntityResponses>(json);
+        string responseJson = File.ReadAllText(Application.dataPath + "/ResponseDataFile.json");
+        _responses = JsonUtility.FromJson<EntityResponses>(responseJson);
 
-        //Get lat long from json
-        string json = File.ReadAllText(Application.dataPath + "/LocationData.json");
-        _location = JsonUtility.FromJson<LocationDataPoints>(json);
+        //Get Locations from json
+        string locationJson = File.ReadAllText(Application.dataPath + "/LocationData.json");
+        _location = JsonUtility.FromJson<LocationDatas>(locationJson);
     }
     public void ReadStringInput()
     {
         //Get Given input text, then find corrosponding category and display returns values.
-        var e = TestNPC.GetResponseFromEntity(inputField.text, _responses);
-        keywordField.text = e.CategoryKey;
-        resposneField.text = e.TextResponse;
+        // To test ResponseDataFile
+        // var e = TestNPC.GetResponseFromEntity(inputField.text, _responses);
+        // keywordField.text = e.CategoryKey;
+        // responseField.text = e.TextResponse;
 
-        Debug.Log(e.TextResponse);
-        Debug.Log(e.CategoryKey);
-        Debug.Log(e.AnimationTrigger);
+        // Debug.Log(e.TextResponse);
+        // Debug.Log(e.CategoryKey);
+        // Debug.Log(e.AnimationTrigger);
+
+        // To Test LocationData.
+        var e = TestNPC.GetLocationFromEntity(inputField.text, _location);
+        keywordField.text = e.name;
+        responseField.text = e.description;
+
     }
     public void SaveToJson()
     {
         //Take given text from fields and save to json as new category.
         EntityResponse data = new EntityResponse();
         data.CategoryKey = keywordField.text;
-        data.TextResponse = resposneField.text;
+        data.TextResponse = responseField.text;
         data.AnimationTrigger = "IsTalking";
         Array.Resize(ref _responses.responses, _responses.responses.Length + 1);
         _responses.responses[_responses.responses.Length - 1] = data;
 
-        string json = JsonUtility.ToJson(_responses, true);
-        File.WriteAllText(Application.dataPath + "/ResponseDataFile.json", json);
+        string responseJson = JsonUtility.ToJson(_responses, true);
+        File.WriteAllText(Application.dataPath + "/ResponseDataFile.json", responseJson);
     }
 
 }
